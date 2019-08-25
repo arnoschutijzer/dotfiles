785ae5832136c7b5d52a7f91f3ea2b2f46b79341
Arno Schutijzer <arno.schutijzer@protonmail.com>
2019-08-26 00:42:24 +0200

    feat: start working on setting up shell script

---
+++ setup-shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: install oh-my-zsh
+
+# TODO: copy over gitster to ~/.oh-my-zsh/custom/themes/gitster.zsh-theme
+
+# TODO: copy over .zshrc to ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
