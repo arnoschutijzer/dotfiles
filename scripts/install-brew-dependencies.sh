06ecb7cf792876d92a2df67761d993bb9fd4e368
Arno Schutijzer <arno.schutijzer@protonmail.com>
2020-08-22 14:59:28 +0200

    add script to install brew dependencies

---
+++ scripts/install-brew-dependencies.sh
@@ -0,0 +1,27 @@
+installing_casks=false
+
+start_installing() {
+  concatenated_deps=""
+  concatenated_casks=""
+
+  while read p; do
+    if [[ $p == *"# brew casks"* ]]; then
+      # skip comments
+      installing_casks=true
+      continue
+    fi
+
+    if [[ $installing_casks == true ]]; then
+      concatenated_casks+="$p "
+    else
+      concatenated_deps+="$p "
+    fi
+  done <brew-dependencies.txt
+
+  brew install $concatenated_deps
+  brew cask install $concatenated_casks
+}
+
+brew upgrade
+start_installing
+brew cleanup
