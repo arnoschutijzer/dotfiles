2f59aacb88a514830f2677b2a677ebf2c6017d2b
Arno Schutijzer <arno.schutijzer@protonmail.com>
2021-04-29 16:14:29 +0200

    add set up shell script

--- setup-shell.sh
+++ setup-shell.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# TODO: install oh-my-zsh
+# see https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# TODO: copy over gitster to ~/.oh-my-zsh/custom/themes/gitster.zsh-theme
-
-# TODO: copy over .zshrc to ~/.zshrc
+ln -sf "$(pwd)"/gitster.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/gitster.zsh-theme
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+ln -sf "$(pwd)"/.zshrc ~/.zshrc
