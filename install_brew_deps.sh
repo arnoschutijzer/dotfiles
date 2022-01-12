3b522d3227c08477af49cbdd7e4b0891eb92e19e
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-01-12 18:53:09 +0100

    feat: set login items

--- install_brew_deps.sh
+++ install_brew_deps.sh
@@ -4,3 +4,9 @@ brew tap bridgecrewio/airiam https://github.com/bridgecrewio/airiam
 while read p; do
   brew install "$p"
 done < "$(pwd)"/configuration/brew-dependencies.txt
+
+# assuming these are all installed by this point...
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/iTerm.app", hidden:true}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Slack.app", hidden:true}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/ShiftIt.app", hidden:true}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/OpenVPN Connect.app", hidden:true}'
