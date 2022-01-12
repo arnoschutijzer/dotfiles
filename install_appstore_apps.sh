83c5eebfbf5c55e49b37ef5f41605d4320f47f2a
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-01-12 19:48:44 +0100

    open spark by default

--- install_appstore_apps.sh
+++ install_appstore_apps.sh
@@ -2,6 +2,7 @@
 
 mas install 1278508951 # trello
 mas install 1176895641 # spark
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spark.app", hidden:true}'
 mas install 552383089 # dashlane
 mas install 747648890 # telegram
 mas install 1295203466 # rdp
