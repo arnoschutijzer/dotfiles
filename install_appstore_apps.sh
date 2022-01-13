50d756b8d92d4b986f7017ca5b7f529ec192f782
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-01-13 13:32:42 +0100

    +harvest

--- install_appstore_apps.sh
+++ install_appstore_apps.sh
@@ -5,3 +5,5 @@ mas install 1176895641 # spark
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spark.app", hidden:true}'
 mas install 747648890 # telegram
 mas install 1295203466 # rdp
+mas install 506189836 # harvest
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Harvest.app", hidden:true}'
