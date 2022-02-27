50c0caee21b7318a5716c0fa788e3ce3f38af8aa
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-02-27 17:52:31 +0100

    dont start harvest on boot

--- install_appstore_apps.sh
+++ install_appstore_apps.sh
@@ -7,4 +7,3 @@ mas install 747648890 # telegram
 mas install 1295203466 # rdp
 mas install 506189836 # harvest
 mas install 517914548 # dashlane
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Harvest.app", hidden:true}'
