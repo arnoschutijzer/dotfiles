b187d05ae6433b6d23fafe316f5d1dca68a7f42e
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-03-04 00:01:13 +0100

    remove unused gitster theme

--- configure_shell.sh
+++ configure_shell.sh
@@ -5,8 +5,6 @@ ln -sf "$(pwd)"/configuration/com.googlecode.iterm2.plist ~/com.googlecode.iterm
 # see https://ohmyz.sh/#install
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ln -sf "$(pwd)"/configuration/gitster.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/gitster.zsh-theme
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 ln -sf "$(pwd)"/configuration/starship.toml ~/.starship.toml
