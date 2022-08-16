66dc73173c35405943a93365101c12726dd2a819
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-08-16 20:49:44 +0200

    spark isn't installed anymore

--- install_appstore_apps.sh
+++ install_appstore_apps.sh
@@ -2,7 +2,6 @@
 
 mas install 1278508951 # trello
 mas install 985367838 # outlook
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spark.app", hidden:true}'
 mas install 747648890 # telegram
 mas install 1295203466 # rdp
 mas install 506189836 # harvest
