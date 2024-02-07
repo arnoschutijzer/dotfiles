56fb88eb573d7b1b5cdc13975f72033d475743dd
Arno Schutijzer <arno.schutijzer@protonmail.com>
2024-02-07 19:02:12 +0100

    clean up

--- install_fonts.sh
+++ install_fonts.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/zed-industries/zed-fonts/releases/download/1.2.0/zed-mono-1.2.0.zip -O zed-mono.zip
 unzip zed-mono.zip zed-mono
+rm zed-mono.zip
 cp -R zed-mono ~/Library/Fonts
