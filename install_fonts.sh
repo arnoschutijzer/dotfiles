1ebec271b540819268511f03a997c8001368ff23
Arno Schutijzer <arno.schutijzer@protonmail.com>
2024-02-09 13:16:50 +0100

    fix install zed-mono

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 wget https://github.com/zed-industries/zed-fonts/releases/download/1.2.0/zed-mono-1.2.0.zip -O zed-mono.zip
-unzip zed-mono.zip zed-mono
-rm zed-mono.zip
-cp -R zed-mono ~/Library/Fonts
+unzip ./zed-mono.zip -d zed-mono
+rm ./zed-mono.zip
+mv -R zed-mono ~/Library/Fonts
