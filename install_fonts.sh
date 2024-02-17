cb16d6292929843333b94794927abba297dfe081
Arno Schutijzer <arno.schutijzer@protonmail.com>
2024-02-17 17:05:54 +0100

    add geist mono

--- install_fonts.sh
+++ install_fonts.sh
@@ -3,4 +3,11 @@
 wget https://github.com/zed-industries/zed-fonts/releases/download/1.2.0/zed-mono-1.2.0.zip -O zed-mono.zip
 unzip ./zed-mono.zip -d zed-mono
 rm ./zed-mono.zip
-mv -R zed-mono ~/Library/Fonts
+rm -rf ~/Library/Fonts/zed-mono
+mv zed-mono ~/Library/Fonts
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/GeistMono.zip -O geist-mono.zip
+unzip ./geist-mono.zip -d geist-mono
+rm ./geist-mono.zip
+rm -rf ~/Library/Fonts/geist-mono
+mv geist-mono ~/Library/Fonts
