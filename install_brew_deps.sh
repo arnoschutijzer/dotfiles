64c2edebecb375ceb607af5ff7f03573c9a28862
Arno Schutijzer <arno.schutijzer@protonmail.com>
2024-02-25 00:23:08 +0100

    shiftit and jumpcut are no longer installed, dont start openvpn on boot

--- install_brew_deps.sh
+++ install_brew_deps.sh
@@ -6,8 +6,5 @@ brew bundle
 jenv enable-plugin maven
 
 # assuming these are all installed by this point...
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Jumpcut.app", hidden:true}'
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/iTerm.app", hidden:true}'
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Slack.app", hidden:true}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/ShiftIt.app", hidden:true}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/OpenVPN Connect.app", hidden:true}'
