6d84f0bbc4f89d40f3ad1b28038c97c854f7add5
Arno Schutijzer <arno.schutijzer@protonmail.com>
2024-02-27 20:14:59 +0100

    update gopls to 0.15.0

--- install_go_deps.sh
+++ install_go_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-go install golang.org/x/tools/gopls@v0.14.2
+go install golang.org/x/tools/gopls@v0.15.0
 go install github.com/go-delve/delve/cmd/dlv@v1.22.0
 go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@v3.1.10
 go install honnef.co/go/tools/cmd/staticcheck@2023.1.7
