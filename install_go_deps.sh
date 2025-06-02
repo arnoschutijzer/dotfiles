c8eb6ff89fd8d82ff371326bf60bdabb3597a75d
Arno Schutijzer <arno.schutijzer@protonmail.com>
2025-06-02 12:27:19 +0200

    feat: upgrade nuclei, gopls, delve and staticcheck

--- install_go_deps.sh
+++ install_go_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-go install golang.org/x/tools/gopls@v0.17.1
-go install github.com/go-delve/delve/cmd/dlv@v1.24.0
-go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@v3.3.7
-go install honnef.co/go/tools/cmd/staticcheck@2024.1.1
+go install golang.org/x/tools/gopls@v0.18.1
+go install github.com/go-delve/delve/cmd/dlv@v1.24.2
+go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@v3.4.4
+go install honnef.co/go/tools/cmd/staticcheck@2025.1.1
