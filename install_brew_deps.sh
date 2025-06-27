2e8533499931861d11f5081f9fc9332efe675936
Arno Schutijzer <arno.schutijzer@protonmail.com>
2025-06-27 10:38:27 +0200

    start ghostty and raycast on boot

--- install_brew_deps.sh
+++ install_brew_deps.sh
@@ -6,5 +6,6 @@ brew bundle
 jenv enable-plugin maven
 
 # assuming these are all installed by this point...
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/iTerm.app", hidden:true}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Ghostty.app", hidden:true}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Raycast.app", hidden:true}'
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Slack.app", hidden:true}'
