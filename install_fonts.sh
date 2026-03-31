7b60b25815c1c16dbc08c128650188cd95e7a069
Arno Schutijzer <arno.schutijzer@protonmail.com>
2026-04-01 01:14:56 +0200

    zed-fonts is archived

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-wget https://github.com/zed-industries/zed-fonts/releases/download/1.2.0/zed-mono-1.2.0.zip -O zed-mono.zip
-unzip ./zed-mono.zip -d zed-mono
-rm ./zed-mono.zip
-rm -rf ~/Library/Fonts/zed-mono
-mv zed-mono ~/Library/Fonts
+wget https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-mono%401.1.0/ibm-plex-mono.zip -O plex-mono.zip
+unzip ./plex-mono.zip -d plex-mono
+rm ./plex-mono.zip
+rm -rf ~/Library/Fonts/plex-mono
+mv plex-mono ~/Library/Fonts
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/GeistMono.zip -O geist-mono.zip
 unzip ./geist-mono.zip -d geist-mono
