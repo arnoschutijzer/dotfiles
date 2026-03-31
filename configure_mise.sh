8fcf3d80853a7ff6e8c3f54d0b0a90d51e7cfcb3
Arno Schutijzer <arno.schutijzer@protonmail.com>
2026-03-31 22:41:04 +0200

    just pull in mise.toml

--- configure_mise.sh
+++ configure_mise.sh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 
-# Settings
-mise settings unset idiomatic_version_file_enable_tools
-mise settings add idiomatic_version_file_enable_tools node
-mise settings add idiomatic_version_file_enable_tools terraform
-mise settings add idiomatic_version_file_enable_tools java
-mise settings set auto_install true
-
-# Global tools
-mise use --global node@22
-mise use --global java@17
-mise use --global maven@3
-mise use --global terraform@1.14
-mise use --global go@1.26
+mkdir -p ~/.config/mise
+ln -sf "$(pwd)"/configuration/mise/config.toml ~/.config/mise/config.toml
+mise install
