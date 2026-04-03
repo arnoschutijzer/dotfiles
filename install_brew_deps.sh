2e58d7e5e08a766487fbdd896a18d2b1042d00ee
Arno Schutijzer <arno.schutijzer@protonmail.com>
2026-04-04 00:07:21 +0200

    jenv was deleted

--- install_brew_deps.sh
+++ install_brew_deps.sh
@@ -3,8 +3,6 @@ cd configuration
 
 brew bundle
 
-jenv enable-plugin maven
-
 # assuming these are all installed by this point...
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Ghostty.app", hidden:true}'
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Raycast.app", hidden:true}'
