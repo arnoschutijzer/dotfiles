24545ac07072c35960d5d8973859bf9e18f3bff3
Arno Schutijzer <arno.schutijzer@protonmail.com>
2026-05-27 12:47:33 +0200

    feat: check before add login items so they're not duplicated

--- install_brew_deps.sh
+++ install_brew_deps.sh
@@ -4,6 +4,14 @@ cd configuration
 brew bundle
 
 # assuming these are all installed by this point...
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Ghostty.app", hidden:true}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Raycast.app", hidden:true}'
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Slack.app", hidden:true}'
+add_login_item() {
+  local name=$1 path=$2
+  local exists=$(osascript -e "tell application \"System Events\" to (login item \"$name\" exists)")
+  if [ "$exists" = "false" ]; then
+    osascript -e "tell application \"System Events\" to make login item at end with properties {path:\"$path\", hidden:true}"
+  fi
+}
+
+add_login_item "Ghostty" "/Applications/Ghostty.app"
+add_login_item "Raycast" "/Applications/Raycast.app"
+add_login_item "Slack" "/Applications/Slack.app"
