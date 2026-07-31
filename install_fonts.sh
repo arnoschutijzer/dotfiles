129b87a4f0e263e5022cc79d17aae9d2ac90e4d2
Arno Schutijzer <arno.schutijzer@protonmail.com>
2026-08-01 01:15:26 +0200

    download fonts with curl instead of wget
    curl ships with macOS, so the fonts no longer depend on the Brewfile
    having completed.

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-wget https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-mono%401.1.0/ibm-plex-mono.zip -O plex-mono.zip
+curl -fsSL https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-mono%401.1.0/ibm-plex-mono.zip -o plex-mono.zip
 unzip ./plex-mono.zip -d plex-mono
 rm ./plex-mono.zip
 rm -rf ~/Library/Fonts/plex-mono
 mv plex-mono ~/Library/Fonts
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/GeistMono.zip -O geist-mono.zip
+curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/GeistMono.zip -o geist-mono.zip
 unzip ./geist-mono.zip -d geist-mono
 rm ./geist-mono.zip
 rm -rf ~/Library/Fonts/geist-mono
